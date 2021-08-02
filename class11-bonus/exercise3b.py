aed79fc4ae0af1d38579f2c5f814549607837359
papri entropy <cosminpetrache4@gmail.com>
2021-08-02 07:26:08 -0700

    class11-bonus ex3b edit

--- class11-bonus/exercise3b.py
+++ class11-bonus/exercise3b.py
@@ -1,23 +1,29 @@
 #!/usr/bin/env python
 
 """
-3a. Retrieve a list of all the devices in NetBox. This will require authentication. 
-As in the previous task, create your headers manually and pass them into your request. 
-In order to perform the NetBox authentication, you should do the following:
+3b. Using the same device information retrieved in exercise 3a, 
+create and print a report to standard output. 
+This report should contain the location, manufacturer, and status for each device. 
+Your output should look similar to the following:
 
-import os
-# Set the token based on the NETBOX_TOKEN environment variable
-token = os.environ["NETBOX_TOKEN"]
+------------------------------------------------------------
+arista1
+----------
+Location: Fremont Data Center
+Vendor: Arista
+Status: Active
+------------------------------------------------------------
 
-Then add the following key to your HTTP Headers:
 
-http_headers["Authorization"] = f"Token {token}"
+------------------------------------------------------------
+arista2
+----------
+Location: Fremont Data Center
+Vendor: Arista
+Status: Active
+------------------------------------------------------------
 
-From this returned data structure (the NetBox "/api/dcim/devices/"), 
-print out all of the device "display_names". 
-Note, the response.json() will contain a "results" key. 
-This "results" key will refer to a list of dictionaries. 
-These dictionaries will contain information about each one of the devices in NetBox.
+...   # remaining devices
 """
 
 import os
