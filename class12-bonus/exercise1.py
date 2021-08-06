e0f120318e3841f7bd49882e4987514f76477b04
papri entropy <cosminpetrache4@gmail.com>
2021-08-06 08:40:00 -0700

    class12-bonus ex1

---
+++ class12-bonus/exercise1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+1a. Copy the "fixme_orig.py" and the "my_devices.py" files from this week's course repository. 
+Execute this "fixme_orig.py" code. It should successfully run and should print out NAPALM device facts. 
+While the code is functional, there are some Python-style issues it.
+
+1b. Run pylint against this "fixme_orig.py" file. 
+Create a copy of fixme_orig.py; name the new file fixme_pylint.py. 
+Fix all of the Pylint errors that are in this fixme_pylint.py file.
+
+1c. Run pycodestyle against "fixme_orig.py". 
+Create a copy of fixme_orig.py; name the new file fixme_pep8.py. 
+Fix all of the pycodestyle errors that are in this fixme_pep8.py file.
+
+1d. Run pylama against the "fixme_pep8.py" file. 
+At this point, Pylama should report no errors. 
+Note that by default, Pylama will run pycodestyle against your code. 
+Consequently, you should see similar warnings and errors as you did when executing pycodestyle. 
+Pylama, however, also supports additional linters which can potentially identify other issues.
+"""
+
+
